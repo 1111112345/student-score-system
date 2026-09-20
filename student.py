49dcb3aea48917d6e1cee36018311c1b1cb16b78
米兰 <makabakam@qq.com>
2026-09-20 18:15:01 +0800

    develop: modify student print line for conflict test

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
-def show_student():
+  def show_student():
     name = input("请输入学生姓名：")
-    print(f"学生姓名：{name}")
+    print(f"【develop分支】学生姓名: {name}")
 
 if __name__ == "__main__":
     show_student()
